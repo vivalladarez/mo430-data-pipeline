ad862da136331e14d3101dc080c07ad24f2b8cd1
Carlos Araki <carlosarak@gmail.com>
2026-04-21 17:06:30 -0300

    feat: template of edge

--- dags/medallion_pipeline_dag.py
+++ dags/medallion_pipeline_dag.py
@@ -16,6 +16,7 @@ from airflow.operators.python import PythonOperator
 from medallion.bronze.bronze import run_bronze
 from medallion.bronze.bronze_ebi import run_bronze_ebi
 from medallion.gold.gold import run_gold_mock_1to1
+from medallion.gold.gold_edge_ppi import run_gold_edge_ppi
 from medallion.silver.silver import run_silver_ebi, run_silver_geo
 
 with DAG(
@@ -46,6 +47,12 @@ with DAG(
         task_id="gold_mock_1to1",
         python_callable=run_gold_mock_1to1,
     )
+    gold_edge_ppi = PythonOperator(
+        task_id="gold_edge_ppi",
+        python_callable=run_gold_edge_ppi,
+    )
 
-    bronze_geo_soft_ingest >> silver_geo >> gold_mock_1to1
+    bronze_geo_soft_ingest >> silver_geo
+    silver_geo >> gold_mock_1to1
+    silver_geo >> gold_edge_ppi
     bronze_ebi_gxa_ingest >> silver_ebi >> gold_mock_1to1
